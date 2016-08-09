d00ba05c8b1f5ada6681b9a4be19f4c6ee7ffda8
FutureGadget <danwoopark@gmail.com>
2016-08-09 13:38:34 +0900

    장바구니 완료

---
+++ javaweb/BookFinal/src/entity/user/UserEntity.java
@@ -0,0 +1,35 @@
+package entity.user;
+
+public class UserEntity {
+	// 데이터베이스의 User 테이블을 보고 만들면 된다.
+	private String uid;
+	private String upw;
+	private String uname;
+	private String uemail;
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getUpw() {
+		return upw;
+	}
+	public void setUpw(String upw) {
+		this.upw = upw;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUemail() {
+		return uemail;
+	}
+	public void setUemail(String uemail) {
+		this.uemail = uemail;
+	}
+	
+	
+}
